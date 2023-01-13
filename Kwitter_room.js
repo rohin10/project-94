83b931c12bb0069c24206e0768b1b147132be499
rohin10 <104300062+rohin10@users.noreply.github.com>
2023-01-13 16:18:35 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC7WAGO7tysV_UurWRa-N5xsmafDoBdyqw",
+    authDomain: "kwitter-22d93.firebaseapp.com",
+    databaseURL: "https://kwitter-22d93-default-rtdb.firebaseio.com",
+    projectId: "kwitter-22d93",
+    storageBucket: "kwitter-22d93.appspot.com",
+    messagingSenderId: "971522832807",
+    appId: "1:971522832807:web:e9cd16031ed38ba9e49e82"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
